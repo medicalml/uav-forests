3debf0a3cc63e657da04e3795f1005fea0477d66
MaczekO <c.matacz@gmail.com>
2020-01-24 21:59:03 +0100

    Forest iterator

--- scripts/forest_iterator_class.py
+++ scripts/forest_iterator_class.py
@@ -4,6 +4,7 @@ import rasterio
 import rasterio.mask
 import numpy as np
 import cv2
+import copy
 
 
 def initiate_geoms(shp):
@@ -22,13 +23,23 @@ class ForestIterator:
         self.shapes_handler = fiona.open(forest_shp_path)
         self.length = len(self.shapes_handler)
 
-    def create_ndvi(self, window):
-        red_channel_img = np.moveaxis(np.stack([self.rgb_tif_handler.read(1,window=window)]), 0, -1)
+    def create_ndvi(self, rows, cols):
+        row_start, col_start = self.rgb_tif_handler.index(rows[0], rows[1])
+        row_stop, col_stop = self.rgb_tif_handler.index(cols[0], cols[1])
+        rgb_win = rasterio.windows.Window.from_slices((row_stop, row_start), (col_start, col_stop))
+
+        row_start, col_start = self.nir_tif_handler.index(rows[0], rows[1])
+        row_stop, col_stop = self.nir_tif_handler.index(cols[0], cols[1])
+        nir_win = rasterio.windows.Window.from_slices((row_stop, row_start), (col_start, col_stop))
+
+        red_channel_img = np.moveaxis(np.stack([self.rgb_tif_handler.read(1, window=rgb_win)]), 0, -1)
         #red_channel_img = np.expand_dims(rgb_img[:, :, 0], axis=-1)
         normalized_red_channel_img = red_channel_img / np.max(red_channel_img)
-        print(normalized_red_channel_img.shape)
-        nir_img = np.moveaxis(np.stack([self.nir_tif_handler.read(1, window=window)]), 0, -1)
-        print(nir_img.shape)
+        nir_img = np.moveaxis(np.stack([self.nir_tif_handler.read(1, window=nir_win,
+                                                                  out_shape=(1,
+                                                                             int(normalized_red_channel_img.shape[0]),
+                                                                             int(normalized_red_channel_img.shape[1]))
+                                                                  )]), 0, -1)
         ndvi = (nir_img - normalized_red_channel_img) / (normalized_red_channel_img + nir_img)
         return ndvi
 
@@ -39,19 +50,23 @@ class ForestIterator:
         y = np.array([a[1] for poly in shp for a in poly])
         row_start, col_start = self.rgb_tif_handler.index(x.min(), y.min())
         row_stop, col_stop = self.rgb_tif_handler.index(x.max(), y.max())
+
         win = rasterio.windows.Window.from_slices((row_stop, row_start), (col_start, col_stop))
         img = np.stack([self.rgb_tif_handler.read(i + 1, window=win) for i in range(3)])
         mask = np.zeros(img.shape[1:], dtype=np.uint8)
+
         for poly in shp:
             joint = [self.rgb_tif_handler.index(l[0], l[1]) for l in poly]
             joint = np.array([[[l[1] - col_start, l[0] - row_stop] for l in joint]], dtype=np.int32)
             cv2.fillPoly(mask, pts=[joint], color=255)
+
         masked = cv2.bitwise_and(img, img, mask=np.stack([mask, mask, mask]))
-        ndvi = self.create_ndvi(win)
+        ndvi = self.create_ndvi((x.min(), y.min()), (x.max(), y.max()))
         print(ndvi.shape, mask.shape)
-        masked_ndvi = cv2.bitwise_and(ndvi, ndvi, mask=mask)
-        return {'rgb':masked,
-                'nir':masked_ndvi,
+        masked_ndvi = cv2.bitwise_and(ndvi, ndvi, mask=np.stack(mask))
+
+        return {'rgb': masked,
+                'nir': np.moveaxis(masked_ndvi,-1,0),
                 'descr':single_shape['properties']}
 
     def __len__(self):
@@ -65,4 +80,4 @@ class ForestIterator:
 if __name__ == '__main__':
     name = 'Swiebodzin'
     path = os.path.join('D:', '_drzewaBZBUAS')
-    shapes = fiona.open(os.path.join(path, 'obszar_' + name.lower() + '.shp'))
+    shapes = os.path.join(path, 'obszar_' + name.lower() + '.shp')
