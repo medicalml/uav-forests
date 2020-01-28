011f1780eccfe68de85fc09a84893b41a13736e8
Michał Kosturek <kosturek.michal@gmail.com>
2020-01-28 22:13:07 +0100

    Added proposed classes signatures

--- src/orthophotomap/forest_segmentation.py
+++ src/orthophotomap/forest_segmentation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class TreeCounter:
+class ForestSegmentation:
 
     def __init__(self, *args, **kwargs):
         '''
@@ -10,24 +10,17 @@ class TreeCounter:
         on every forest part, and any required
         initialisation.
         '''
-        pass
+    pass
 
-    def count(self, rgb_image: np.ndarray, ndvi_image: np.ndarray,
-              forest_mask: np.ndarray):
+    def mask(self, rgb_image: np.ndarray, ndvi_image: np.ndvi_image):
         assert 3 == len(rgb_image.shape), \
             "RGB image array should be 3-dimensional"
         assert 2 == len(ndvi_image.shape), \
             "NDVI image array should be 2-dimensional"
-        assert 2 == len(forest_mask.shape), \
-            "Forest mask array should be 2-dimensional"
         assert rgb_image.shape[:2] == ndvi_image.shape, \
             "NDVI image should have the same height and width as RGB"
-        assert rgb_image.shape[:2] == forest_mask.shape, \
-            "Forest mask should have the same height and width as RGB"
 
         # ...
         # ...
         # ...
-        trees_points = [(14, 20), (444, 94)]  # (row, column)
-        count = 17
-        return {"trees": trees_points, "count": count}
+        return np.ones(ndvi_image.shape)
