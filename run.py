c43e4c92669c3ef1ec7e52b42e819de8aa1b5348
robosyn <michal.miotk@pwr.edu.pl>
2020-02-13 23:12:21 +0100

    working

--- run.py
+++ run.py
@@ -9,7 +9,9 @@ multi_folder = 'data_out'
 
 tile_size = 256
 step = 256
-common_arg = ['python3', 'scripts/orthophotomap_ir_to_patches_dataset.py' ,'--geotiff', '/home/h/ML dane dla kola/Swiebodzin/RGB_Swiebodzin.tif', '--nirtiff', "/home/h/ML dane dla kola/Swiebodzin/NIR_Swiebodzin.tif" ,'--shapefile', '/home/h/ML dane dla kola/Swiebodzin/drzewa_swiebodzin.shp', '--tile-size='+str(tile_size), '--step='+str(step), '--verbose', '--single-process']
+common_arg = ['python3', 'scripts/orthophotomap_ir_to_patches_dataset.py' ,'--geotiff', 
+'/home/h/_drzewaBZBUAS/RGB_szprotawa_transparent_mosaic_group1.tif', '--nirtiff',
+"/home/h/_drzewaBZBUAS/Szprotawa_translated_EVI.tif" ,'--shapefile', '/home/h/_drzewaBZBUAS/szprotawa_shp/drzewa_szprotawa.shp', '--tile-size='+str(tile_size), '--step='+str(step), '--verbose', '--single-process']
 
 multi_proc = ['--target-dir='+multi_folder]
 
