333ed19878be0244460ba0cc7ab9aaab3482b7e9
Michał Kosturek <kosturek.michal@gmail.com>
2020-02-24 17:48:35 +0100

    refactoring and bug fixes

--- src/utils/training.py
+++ src/utils/training.py
@@ -1,6 +1,7 @@
 from detectron2.data import detection_utils as utils
 import os
 import cv2
+import pickle
 import pandas as pd
 import geopandas as gpd
 import numpy as np
@@ -21,106 +22,37 @@ from detectron2.evaluation import COCOEvaluator
 from detectron2.engine import DefaultTrainer
 from detectron2.config import CfgNode
 
-
-from sklearn.model_selection import train_test_split
+from src.utils.dataset import split_train_val_test
+from src.utils.augmenter import Augmenter
 
 from typing import List, Dict
 
 
-class Augmenter:
+def get_splits_for_multipart_dataset(base_path, train_val_maps, test_maps):
 
-    def __init__(self, flip_probability=0.5,
-                 contrast_probability=0.2,
-                 defocus_probability=0.1,
-                 motion_blur_probability=0.2,
-                 cutout_probability=0.2,
-                 scale_probability=0.1,
-                 shift_probability=0.2,
-                 fog_probability=0.1):
-        rot = iaa.Rot90(ia.ALL)
-        shift = iaa.Affine(translate_percent=(-0.2, 0.2))
-        flip = iaa.OneOf([iaa.Fliplr(1), iaa.Flipud(1)])
-        scale = iaa.Affine(scale=(0.75, 1.25))
-        motion_blur = iaa.MotionBlur()
-        cutout = iaa.Cutout(
-            nb_iterations=(1, 3),
-            size=(0.01, 0.1),
-            squared=False)
-        fog = iaa.imgcorruptlike.Fog(severity=1)
-        defocus = iaa.imgcorruptlike.DefocusBlur(severity=1)
-        contrast = iaa.imgcorruptlike.Contrast(severity=1)
+    if os.path.exists(f"{base_path}/train_val_test_splits.pkl"):
+        print("Reusing existing splits_file")
+        with open(f"{base_path}/train_val_test_splits.pkl", "rb") as f:
+            splits = pickle.load(f)
+    else:
+        splits = {'train': {}, 'val': {}, 'test': {}}
+        for part in train_val_maps:
+            samples = pd.read_pickle(
+                f"{base_path}/{part}/annotation.pkl")["patch_number"].unique()
+            part_splits = split_train_val_test(samples, 0.8, 0.2, 0)
+            for d in part_splits:
+                splits[d][part] = part_splits[d]
 
-        aug_weather = iaa.Sequential([
-            iaa.Sometimes(fog_probability, fog),
-        ], random_order=True)
+        for part in test_maps:
+            samples = pd.read_pickle(
+                f"{base_path}/{part}/annotation.pkl")["patch_number"].unique()
+            part_splits = {'train': [], 'val': [], 'test': samples}
+            for d in part_splits:
+                splits[d][part] = part_splits[d]
 
-        aug_initial = iaa.Sequential([
-            rot,
-            iaa.Sometimes(flip_probability, flip),
-            iaa.Sometimes(contrast_probability, contrast)
-        ], random_order=True)
-
-        aug_camera = iaa.Sequential([
-            iaa.Sometimes(defocus_probability, defocus),
-            iaa.Sometimes(motion_blur_probability, motion_blur),
-        ], random_order=False)
-
-        aug_obstacles = iaa.Sequential([
-            iaa.Sometimes(cutout_probability, cutout),
-            iaa.Sometimes(scale_probability, scale),
-            iaa.Sometimes(shift_probability, shift)
-        ], random_order=False)
-
-        self.augmentation_pipeline = iaa.Sequential(
-            [aug_weather, aug_initial, aug_camera, aug_obstacles],
-            random_order=False)
-
-    def augment(self, rgb_image: np.ndarray,
-                bounding_boxes: List[Dict],
-                ndvi_image: np.ndarray = None,
-                forest_mask: np.ndarray = None):
-        assert all(bb['bbox_mode'] == BoxMode.XYXY_ABS
-                   for bb in bounding_boxes), \
-            "Unsupported bbox_mode"
-
-        bbox_on_image = BoundingBoxesOnImage([
-            BoundingBox(x1=annotation["bbox"][0],
-                        y1=annotation["bbox"][1],
-                        x2=annotation["bbox"][2],
-                        y2=annotation["bbox"][3],
-                        label=annotation["category_id"])
-            for annotation in bounding_boxes
-        ], rgb_image.shape)
-
-        hmap_on_image = None
-        if ndvi_image is not None:
-            hmap_on_image = HeatmapsOnImage(ndvi_image.astype(np.float32), rgb_image.shape,
-                                            min_value=ndvi_image.min(),
-                                            max_value=ndvi_image.max())
-        smap_on_image = None
-        if forest_mask is not None:
-            smap_on_image = SegmentationMapsOnImage(forest_mask,
-                                                    rgb_image.shape)
-
-        (aug_img, aug_smap,
-         aug_hmap, aug_bbox) = self.augmentation_pipeline(image=rgb_image,
-                                                          segmentation_maps=smap_on_image,
-                                                          heatmaps=hmap_on_image,
-                                                          bounding_boxes=bbox_on_image)
-
-        return {"rgb_image": aug_img,
-                "mask": aug_smap and aug_smap.arr.squeeze(),
-                "ndvi": aug_hmap and aug_hmap.get_arr().squeeze(),
-                "annotations": [{"bbox": [bb.x1_int, bb.y1_int, bb.x2_int, bb.y2_int],
-                                 "bbox_mode": BoxMode.XYXY_ABS,
-                                 "category_id": bb.label}
-                                for bb in aug_bbox.clip_out_of_image()]}
-
-    def __call__(self, rgb_image: np.ndarray,
-                 bounding_boxes: List[Dict],
-                 ndvi_image: np.ndarray = None,
-                 forest_mask: np.ndarray = None):
-        return self.augment(rgb_image, bounding_boxes, ndvi_image, forest_mask)
+        with open(f"{base_path}/train_val_test_splits.pkl", "wb") as f:
+            pickle.dump(splits, f)
+    return splits
 
 
 class SickTreesDatasetMapper:
@@ -138,14 +70,15 @@ class SickTreesDatasetMapper:
 
         image = cv2.imread(data_dict["file_name"], cv2.IMREAD_UNCHANGED)
         image = image[:, :, :self.nb_channels]
+
         # if image.shape[2] == 4:
         #     conversion = cv2.COLOR_BGRA2RGBA
         # else:
         #     conversion = cv2.COLOR_BGR2RGB
         # image = cv2.cvtColor(image, conversion)
 
-        data_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1)
-                                             .astype("float32"))
+        # data_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1)
+        #                                      .astype("float32"))
 
         if self.augmenter:
             rgb_image = image[:, :, :3]
@@ -154,7 +87,7 @@ class SickTreesDatasetMapper:
                 ndvi_image = image[:, :, 3]
 
             data_dict["original_annotations"] = data_dict["annotations"]
-            data_dict["original_image"] = data_dict["image"]
+            data_dict["original_image"] = image
 
             augmented = self.augmenter(rgb_image,
                                        data_dict["annotations"],
@@ -165,6 +98,11 @@ class SickTreesDatasetMapper:
                 image = np.dstack([augmented["rgb_image"], augmented["ndvi"]])
             else:
                 image = augmented["rgb_image"]
+
+        if self.cfg.INPUT.FORMAT == "BGR":
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        elif self.cfg.INPUT.FORMAT == "BGRN":
+            image = cv2.cvtColor(image, cv2.COLOR_RGBA2BGRA)
 
         data_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1)
                                              .astype("float32"))
@@ -189,11 +127,14 @@ class SickTreesAugmentedTrainer(DefaultTrainer):
     @classmethod
     def build_train_loader(cls, cfg):
         return build_detection_train_loader(cfg, mapper=SickTreesDatasetMapper(cfg, is_train=True,
-                                                                               nb_channels=4,
+                                                                               nb_channels=3,
                                                                                augmenter=Augmenter()))
 
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            os.makedirs(cfg.OUTPUT_DIR+f"/eval/{dataset_name}", exist_ok=True)
+
         return COCOEvaluator(dataset_name, cfg, False, output_dir=cfg.OUTPUT_DIR+f"/eval/{dataset_name}")
 
 
