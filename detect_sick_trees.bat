689e61e29df90ef81a036433d92e21b5ef8379ea
mkosturek <35629146+mkosturek@users.noreply.github.com>
2020-03-06 18:22:05 +0100

    Create detect_sick_trees.bat

---
+++ detect_sick_trees.bat
@@ -0,0 +1,4 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3\envs\geopython37
+cd C:\uav-forests
+C:\ProgramData\Anaconda3\envs\geopython37\python.exe GUI_generate_shapefile_with_sick_trees_detections.py
+PAUSE
