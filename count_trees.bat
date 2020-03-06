f16b584ee4f97f38984ff0facae3dfa0613a1538
mkosturek <35629146+mkosturek@users.noreply.github.com>
2020-03-06 18:19:36 +0100

    Create count_trees.bat

---
+++ count_trees.bat
@@ -0,0 +1,4 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3\envs\geopython37
+cd C:\uav-forests-master 
+C:\ProgramData\Anaconda3\envs\geopython37\python.exe GUI_generate_shapefile_with_trees_positions.py
+PAUSE
