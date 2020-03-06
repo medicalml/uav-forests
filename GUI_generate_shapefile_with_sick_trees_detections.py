6f0dad67666e1b86ad9c48b0a425d65808d4a118
tugot17 <piotr635@gmail.com>
2020-03-06 07:51:53 +0100

    GUIs updated

---
+++ GUI_generate_shapefile_with_sick_trees_detections.py
@@ -0,0 +1,46 @@
+from gooey import GooeyParser
+import os
+from gooey import Gooey
+
+from generate_shapefile_with_sick_trees_detections import perform_sick_tree_detection
+
+
+@Gooey
+def main():
+
+    default_weights_path = os.path.join(os.getcwd(), "model", "model_weights.pth")
+    default_config_path = os.path.join(os.getcwd(), "model", "config.yml")
+
+    parser = GooeyParser(description = "Tree counting app")
+    parser.add_argument('geotiff', help="Path to RGG geotiff", widget='FileChooser', )
+
+    parser.add_argument('shapefile', help="Patch to shapefile", widget='FileChooser', )
+
+    parser.add_argument('target_dir', help="Where should result files be saved", widget='DirChooser', )
+
+    parser.add_argument("--config_file", widget='FileChooser', help="Neural Netowork configuration", default=default_config_path)
+
+    parser.add_argument("--weights", widget='FileChooser', help="Neural Netowork weighs file", default=default_weights_path)
+
+    parser.add_argument("--cpu", dest="device", action="store_true", default=False,
+                        help="Should programme use CPU insted of GPU")
+    parser.add_argument("--threshold", default=0.4, type=float,
+                        help="thresold for sick trees detctions")
+    parser.add_argument(
+        "--no_masking",
+        default=False,
+        action="store_true",
+        help="Should Counting mask be suspended"
+    )
+    parser.add_argument('--start_id', help="First Area id to count trees in", default="0")
+    parser.add_argument('--end_id', help="Last Area id to count trees in", default=-1)
+    # parser.add_argument('index', help="Parameter table index name", default="id_ob")
+
+
+    args = parser.parse_args()
+    args.start_id = int(args.start_id)
+
+    perform_sick_tree_detection(args)
+
+if __name__ == '__main__':
+    main()
--- GUI_generate_shapefile_with_trees_positions.py
+++ GUI_generate_shapefile_with_trees_positions.py
@@ -10,18 +10,18 @@ def main():
     parser.add_argument('geotiff', help="Path to RGG geotiff", widget='FileChooser', )
     parser.add_argument('shapefile', help="Patch to shapefile", widget='FileChooser', )
     parser.add_argument('target_dir', help="Where should result files be saved", widget='DirChooser', )
-    parser.add_argument('window_size', help="size of a window on which program should count trees", default=128)
+    parser.add_argument('--window_size', help="size of a window on which program should count trees", default=128)
     parser.add_argument(
         "--no_masking",
         default=False,
         action="store_true",
         help="Should Counting mask be suspended"
     )
-    parser.add_argument('minimal_size', help="Minimal tree area in px on the rgb image", default=36)
-    parser.add_argument('brightness', help="Adjust brightness parameter of counting", default=1)
-    parser.add_argument('start_id', help="First Area id to count trees in", default="0")
-    parser.add_argument('end_id', help="Last Area id to count trees in", default=-1)
-    parser.add_argument('index', help="Parameter table index name", default="id_ob")
+    parser.add_argument('--minimal_size', help="Minimal tree area in px on the rgb image", default=36)
+    parser.add_argument('--brightness', help="Adjust brightness parameter of counting", default=1)
+    parser.add_argument('--start_id', help="First Area id to count trees in", default="0")
+    parser.add_argument('--end_id', help="Last Area id to count trees in", default=-1)
+    parser.add_argument('--index', help="Parameter table index name", default="id_ob")
 
 
     args = parser.parse_args()
